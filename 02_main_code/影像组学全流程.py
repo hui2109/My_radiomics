3df9ccf1f517809453a5af57ab4ef3b912a87523
辉哥 <995631664@qq.com>
2023-05-14 23:30:21 +0800

    代码优化

--- 02_main_code/影像组学全流程.py
+++ 02_main_code/影像组学全流程.py
@@ -30,57 +30,80 @@ def _extract_features(img, mask):
         Original={},
         LoG={},
         Wavelet={},
-        Square={},
-        SquareRoot={},
-        Logarithm={},
-        Exponential={},
-        Gradient={},
+        # Square={},
+        # SquareRoot={},
+        # Logarithm={},
+        # Exponential={},
+        # Gradient={},
         LBP2D={}
     )
 
     return extractor.execute(img, mask)
 
 
+def init_info():
+    with open('./info.txt', 'a', 1, 'utf-8') as f:
+        f.write(time.strftime('%Y-%m-%d %H:%M:%S'))
+        f.write('\n')
+
+
+def info_wt(*msg):
+    with open('./info.txt', 'a', 1, 'utf-8') as f:
+        for i in msg:
+            f.write(str(i))
+            f.write('\n')
+        f.write('\n')
+
+
 class RadiomicsAnalysis:
     def __init__(self):
         pass
 
-    def svm_route(self):
-        self.batch_extract()
+    def init_route(self):
+        # self.batch_extract()
         self.data_preprocess()
         self.t_test()
         self.lasso()
-        self.svm()
-        self.feature_weight_figure()
-        self.feature_correlation_figure()
-        self.lambda_selection_figure()
-        self.lambda_coefficient_figure()
-        self.roc_curve_figure()
+
+    def svm_route(self):
+        init_info()
+        info_wt('svm_route')
+        self.svm(model='svm')
+        self.feature_weight_figure(model='svm')
+        self.feature_correlation_figure(model='svm')
+        self.lambda_selection_figure(model='svm')
+        self.lambda_coefficient_figure(model='svm')
+        self.roc_curve_figure(model='svm')
 
     def rf_route(self):
-        self.batch_extract()
-        self.data_preprocess()
-        self.t_test()
-        self.lasso()
-        self.random_forest()
-        self.feature_weight_figure()
-        self.feature_correlation_figure()
-        self.lambda_selection_figure()
-        self.lambda_coefficient_figure()
-        self.roc_curve_figure()
+        init_info()
+        info_wt('rf_route')
+        self.random_forest(model='rf')
+        self.feature_weight_figure(model='rf')
+        self.feature_correlation_figure(model='rf')
+        self.lambda_selection_figure(model='rf')
+        self.lambda_coefficient_figure(model='rf')
+        self.roc_curve_figure(model='rf')
 
-    def pca_route(self):
-        self.batch_extract()
-        self.data_preprocess()
-        self.t_test()
-        self.lasso()
-        self.pca()
-        self.random_forest()
-        self.feature_weight_figure()
-        self.feature_correlation_figure()
-        self.lambda_selection_figure()
-        self.lambda_coefficient_figure()
-        self.roc_curve_figure()
+    def pca_rf_route(self):
+        init_info()
+        info_wt('pca_rf_route')
+        self.random_forest(model='pca_rf')
+        self.feature_weight_figure(model='pca_rf')
+        self.feature_correlation_figure(model='pca_rf')
+        self.lambda_selection_figure(model='pca_rf')
+        self.lambda_coefficient_figure(model='pca_rf')
+        self.roc_curve_figure(model='pca_rf')
+
+    def pca_svm_route(self):
+        init_info()
+        info_wt('pca_svm_route')
+        self.svm(model='pca_svm')
+        self.feature_weight_figure(model='pca_svm')
+        self.feature_correlation_figure(model='pca_svm')
+        self.lambda_selection_figure(model='pca_svm')
+        self.lambda_coefficient_figure(model='pca_svm')
+        self.roc_curve_figure(model='pca_svm')
 
     def _image_to_nrrd(self):
         if not os.path.exists('dataset_voc/'):
@@ -129,6 +152,7 @@ class RadiomicsAnalysis:
                         # 将数组写入文件
                         if not os.path.exists('./nrrd_file/'):
                             os.makedirs('./nrrd_file/')
+
                         sitk.WriteImage(img, f'./nrrd_file/{i.split(".")[0]}_img.nrrd')
                         sitk.WriteImage(mask, f'./nrrd_file/{j.split(".")[0]}_mask_{roi_value}.nrrd')
 
@@ -170,7 +194,8 @@ class RadiomicsAnalysis:
 
     def data_preprocess(self, start_column_num=22):
         # 数据预处理
-        self.data = pd.read_excel(self.filename).iloc[:, start_column_num:]
+        # self.data = pd.read_excel(self.filename).iloc[:, start_column_num:]
+        self.data = pd.read_excel('./nrrd_file/results_1680691841.xlsx').iloc[:, start_column_num:]
 
         # 数据集划分
         # test_size 7 3 分, random_state 每次运行都是一个稳定的结果
@@ -195,8 +220,8 @@ class RadiomicsAnalysis:
             else:
                 if ttest_ind(self.data_train_a[colName], self.data_train_b[colName], equal_var=False)[1] < 0.05:
                     index.append(colName)
-        print('t检验后, 筛选出', len(index), '个特征')
-        print('这些特征分别是:', index)
+        info_wt('t检验后, 筛选出', len(index), '个特征')
+        info_wt('这些特征分别是:', index)
 
         # t检验后训练集数据整理
         data_train_a = self.data_train_a[index]
@@ -229,12 +254,12 @@ class RadiomicsAnalysis:
 
     def lasso(self):
         # LASSO特征筛选
-        self.alphas = np.logspace(-4, 1, 50)  # alpha就是损失函数的拉姆达， 从10的-4次方到10的一次方均匀地取50个值
+        self.alphas = np.logspace(-3, 1, 50)  # alpha就是损失函数的拉姆达， 从10的-4次方到10的1次方均匀地取50个值
         self.model_lassoCV = LassoCV(alphas=self.alphas, max_iter=100000).fit(self.X_train, self.y_train)
         self.coef = pd.Series(self.model_lassoCV.coef_, index=self.X_train.columns)  # coef_代表LASSO计算出来的每个特征的系数
-        print('最优alpha值为:', self.model_lassoCV.alpha_)  # alpha代表选出来的最优alpha值
-        print('%s %d' % ('Lasso选择出的系数不为0的特征的个数:', sum(self.coef != 0)))
-        print('Lasso选择出的系数不为0的特征:\n', self.coef[self.coef != 0], sep='')
+        info_wt('最优alpha值为:', self.model_lassoCV.alpha_)  # alpha代表选出来的最优alpha值
+        info_wt('%s %d' % ('Lasso选择出的系数不为0的特征的个数:', sum(self.coef != 0)))
+        info_wt('Lasso选择出的系数不为0的特征:', self.coef[self.coef != 0])
         self.index = self.coef[self.coef != 0].index
         self.X_train_raw = self.X_train
         self.X_train = self.X_train[self.index]
@@ -244,14 +269,19 @@ class RadiomicsAnalysis:
         # 主成分分析
         model_pca = PCA(n_components=0.99)  # n_components代表降维后的特征至少能解释原始特征99%的方差
         model_pca.fit(self.X_train)
+        index = self.X_train.columns
         self.X_train = model_pca.fit_transform(self.X_train)
+        self.X_train = pd.DataFrame(self.X_train)
         self.X_test = model_pca.transform(self.X_test)
-        print(f'训练集中PCA降维前的数据形状为:{self.X_train_raw.shape}, PCA降维后的数据形状为:{self.X_train.shape}')
-        print('训练集中降维后每个特征的方差', model_pca.explained_variance_)
-        print('训练集中降维后每个特征能够解释原始信息的百分比', model_pca.explained_variance_ratio_)
-        print('训练集中降维后所有特征总共能解释原始信息的百分比', sum(model_pca.explained_variance_ratio_))
+        self.X_test = pd.DataFrame(self.X_test)
 
-    def random_forest(self):
+        info_wt(
+            f'训练集中PCA降维前的数据形状为:{self.X_train_raw.shape}, PCA降维后的数据形状为:{self.X_train.shape}')
+        info_wt('训练集中降维后每个特征的方差', model_pca.explained_variance_)
+        info_wt('训练集中降维后每个特征能够解释原始信息的百分比', model_pca.explained_variance_ratio_)
+        info_wt('训练集中降维后所有特征总共能解释原始信息的百分比', sum(model_pca.explained_variance_ratio_))
+
+    def random_forest(self, model: str):
         # 随机森林分类器
         self.model = RandomForestClassifier(
             n_estimators=200,  # 设置随机森林中一共有多少颗树, 默认是100
@@ -259,15 +289,16 @@ class RadiomicsAnalysis:
             random_state=20,  # 每次运行都是一个稳定的结果, 默认是None
             class_weight='balanced')  # 类别的权重, 目的是让0类和1类的数据平衡, 默认是None
         self.model.fit(self.X_train, self.y_train)
-        print('测试集上的平均准确率为:', self.model.score(self.X_test, self.y_test))
-        print('测试集的正确结果为:', list(self.y_test))
-        print('对测试集进行结果预测的展示:', self.model.predict(self.X_test))
-        print('测试集中每种预测结果的概率展示:', self.model.predict_proba(self.X_test))
-        print('在拟合过程中用到了多少特征:', self.model.n_features_in_)
-        print('用到的每个特征的权重:', self.model.feature_importances_)
-        print('展示随机森林模型的各个参数:', self.model.get_params())
+        info_wt(f'{model}_训练集上的平均准确率为:', self.model.score(self.X_train, self.y_train))
+        info_wt(f'{model}_测试集上的平均准确率为:', self.model.score(self.X_test, self.y_test))
+        info_wt(f'{model}_测试集的正确结果为:', list(self.y_test))
+        info_wt(f'{model}_对测试集进行结果预测的展示:', self.model.predict(self.X_test))
+        info_wt(f'{model}_测试集中每种预测结果的概率展示:', self.model.predict_proba(self.X_test))
+        info_wt(f'{model}_在拟合过程中用到了多少特征:', self.model.n_features_in_)
+        info_wt(f'{model}_用到的每个特征的权重:', self.model.feature_importances_)
+        info_wt(f'{model}_展示随机森林模型的各个参数:', self.model.get_params())
 
-    def svm(self):
+    def svm(self, model: str):
         # 支持向量机分类器
         self.model = SVC(kernel='rbf',  # rbf：径向基核函数，常用的核函数有：
                          # 线性核函数（linear），多项式核函数（poly），径向基核函数（rbf），sigmoid核函数
@@ -277,14 +308,15 @@ class RadiomicsAnalysis:
                          random_state=20,  # 每次运行都是一个稳定的结果, 默认是None
                          )
         self.model.fit(self.X_train, self.y_train)
-        print('测试集上的平均准确率为:', self.model.score(self.X_test, self.y_test))
-        print('测试集的正确结果为:', list(self.y_test))
-        print('对测试集进行结果预测的展示:', self.model.predict(self.X_test))
-        print('测试集中每种预测结果的概率展示:', self.model.predict_proba(self.X_test))
-        print('在拟合过程中用到了多少特征:', self.model.n_features_in_)
-        print('展示支持向量机模型的各个参数:', self.model.get_params())
+        info_wt(f'{model}_训练集上的平均准确率为:', self.model.score(self.X_train, self.y_train))
+        info_wt(f'{model}_测试集上的平均准确率为:', self.model.score(self.X_test, self.y_test))
+        info_wt(f'{model}_测试集的正确结果为:', list(self.y_test))
+        info_wt(f'{model}_对测试集进行结果预测的展示:', self.model.predict(self.X_test))
+        info_wt(f'{model}_测试集中每种预测结果的概率展示:', self.model.predict_proba(self.X_test))
+        info_wt(f'{model}_在拟合过程中用到了多少特征:', self.model.n_features_in_)
+        info_wt(f'{model}_展示支持向量机模型的各个参数:', self.model.get_params())
 
-    def feature_weight_figure(self):
+    def feature_weight_figure(self, model):
         # 特征权重图
         plt.figure(figsize=(12, 10), dpi=80)
         x_values = np.arange(len(self.index))
@@ -304,10 +336,14 @@ class RadiomicsAnalysis:
         )
         plt.xlabel("feature")  # 横轴名称
         plt.ylabel("weight")  # 纵轴名称
-        plt.title('Weight of features', fontsize=22)
+        plt.title(f'{model}_Weight of features', fontsize=22)
+        # 创建结果图文件夹
+        if not os.path.exists('./saved_images/'):
+            os.makedirs('./saved_images/')
+        plt.savefig(f'./saved_images/特征权重图_{model}.jpg', bbox_inches='tight', dpi=300)
         plt.show()
 
-    def feature_correlation_figure(self):
+    def feature_correlation_figure(self, model: str):
         # 特征相关性热度图
         plt.figure(figsize=(12, 10), dpi=80)
         sns.heatmap(
@@ -318,12 +354,16 @@ class RadiomicsAnalysis:
             center=0.5,
             annot=True
         )
-        plt.title('Correlogram of features', fontsize=22)
+        plt.title(f'{model}_Correlogram of features', fontsize=22)
         plt.xticks(fontsize=12)
         plt.yticks(fontsize=12)
+        # 创建结果图文件夹
+        if not os.path.exists('./saved_images/'):
+            os.makedirs('./saved_images/')
+        plt.savefig(f'./saved_images/特征相关性热度图_{model}.jpg', bbox_inches='tight', dpi=300)
         plt.show()
 
-    def lambda_selection_figure(self):
+    def lambda_selection_figure(self, model: str):
         # LASSO模型中Lambda选值图
         MSEs = self.model_lassoCV.mse_path_
         MSEs_mean = np.apply_along_axis(np.mean, 1, MSEs)
@@ -346,10 +386,14 @@ class RadiomicsAnalysis:
         plt.axvline(self.model_lassoCV.alpha_, color='black', ls="--")
         plt.xlabel('Lambda')
         plt.ylabel('MSE')
-        plt.title('Selection of lambda', fontsize=22)
+        plt.title(f'{model}_Selection of lambda', fontsize=22)
+        # 创建结果图文件夹
+        if not os.path.exists('./saved_images/'):
+            os.makedirs('./saved_images/')
+        plt.savefig(f'./saved_images/LASSO模型中Lambda选值图_{model}.jpg', bbox_inches='tight', dpi=300)
         plt.show()
 
-    def lambda_coefficient_figure(self):
+    def lambda_coefficient_figure(self, model: str):
         # 特征系数随Lambda变化曲线
         coef = self.model_lassoCV.path(self.X_train_raw, self.y_train, alphas=self.alphas, max_iter=100000)[1].T
         plt.figure(dpi=80)
@@ -357,10 +401,14 @@ class RadiomicsAnalysis:
         plt.axvline(self.model_lassoCV.alpha_, color='black', ls="--")
         plt.xlabel('Lambda')
         plt.ylabel('Coefficients')
-        plt.title('Coefficient with lambda', fontsize=22)
+        plt.title(f'{model}_Coefficient with lambda', fontsize=22)
+        # 创建结果图文件夹
+        if not os.path.exists('./saved_images/'):
+            os.makedirs('./saved_images/')
+        plt.savefig(f'./saved_images/特征系数随Lambda变化曲线_{model}.jpg', bbox_inches='tight', dpi=300)
         plt.show()
 
-    def roc_curve_figure(self):
+    def roc_curve_figure(self, model: str):
         y_test_probs = self.model.predict_proba(self.X_test)
         fpr, tpr, thresholds = roc_curve(self.y_test, y_test_probs[:, 1], pos_label=1)
         plt.figure()
@@ -372,15 +420,26 @@ class RadiomicsAnalysis:
         auc_score = roc_auc_score(self.y_test, y_test_pred)
 
         # ROC曲线的AUC
-        print('ROC曲线的AUC为:', auc_score)
+        info_wt(f'{model}_ROC曲线的AUC为:', auc_score)
         #  精确度（Precision），敏感度（Sensitivity），特异度（Specificity）等输出
         #  将“1”类作为“阳性”时，“1”类的recall就是Sensitivity，“0”类的recall即为Specificity
-        print('分类报告如下:\n', classification_report(self.y_test, y_test_pred), sep='')
+        info_wt(f'{model}_分类报告如下:', classification_report(self.y_test, y_test_pred))
 
-        plt.title('ROC Curve', fontsize=22)
+        plt.title(f'{model}_ROC Curve', fontsize=22)
+        # 创建结果图文件夹
+        if not os.path.exists('./saved_images/'):
+            os.makedirs('./saved_images/')
+        plt.savefig(f'./saved_images/ROC曲线_{model}.jpg', bbox_inches='tight', dpi=300)
         plt.show()
 
 
 if __name__ == '__main__':
     ra = RadiomicsAnalysis()
+    ra.init_route()
     ra.svm_route()
+    ra.rf_route()
+    ra.pca()
+    ra.pca_svm_route()
+    ra.pca_rf_route()
+    # ra.init_route()
+    # ra.pca()
